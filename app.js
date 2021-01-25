a10d853e9ef4cfd36f4e090bc4bc7645f4fcad50
mpagung <michaelputra.agung@gmail.com>
2021-01-26 07:19:33 +0800

    Added posting feature and displaying the title and body in the home page

--- app.js
+++ app.js
@@ -9,18 +9,24 @@ const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pelle
 const contactContent = "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+const posts=[];
 
 app.set('view engine', 'ejs');
 
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(express.static("public"));
 
+
+
+
 app.get("/",function(req,res){
   res.render("home",{
     content1:homeStartingContent,
+    post:posts
     // content2:aboutContent,
     // content3:contactContent
   })
+  console.log("In get, posts:"+posts[0]+posts[1]);
 })
 
 app.get("/about",function(req,res){
@@ -40,22 +46,21 @@ app.get("/contact",function(req,res){
 })
 
 app.get("/compose",function(req,res){
+  // console.log("hello")
   res.render("compose")
 })
 
 app.post("/compose",function(req,res){
-  console.log(req.body.postTitle)
+
+  const post={
+    title:req.body.postTitle,
+    body:req.body.postValue
+  };
+  posts.push(post);
+
+  // console.log(posts);
+  res.redirect("/");
 })
-
-
-
-
-
-
-
-
-
-
 
 
 app.listen(3000, function() {
