7274a60156114e3a8ba8448a5a2d8c7be3483b16
mpagung <michaelputra.agung@gmail.com>
2021-01-26 20:39:23 +0800

    add post feature to view each post separately based on the selected post by using the url of the title

--- app.js
+++ app.js
@@ -3,6 +3,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const lodash = require("lodash");
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -26,7 +27,6 @@ app.get("/",function(req,res){
     // content2:aboutContent,
     // content3:contactContent
   })
-  console.log("In get, posts:"+posts[0]+posts[1]);
 })
 
 app.get("/about",function(req,res){
@@ -60,6 +60,27 @@ app.post("/compose",function(req,res){
 
   // console.log(posts);
   res.redirect("/");
+})
+
+app.get("/posts/:postName", function(req,res){
+  const postName=lodash.lowerCase(req.params.postName);
+
+
+  posts.forEach(function(post){
+    console.log("url:"+postName+",title:"+lodash.lowerCase(post.title));
+    if (postName===lodash.lowerCase(post.title)){
+      console.log("Match found!")
+      res.render("post",{
+        post:post
+      })
+    } else {
+      console.log("Not a match!")
+    }
+  })
+
+  // res.render("post",{
+  //   postName:req.params.postName
+  // })
 })
 
 
